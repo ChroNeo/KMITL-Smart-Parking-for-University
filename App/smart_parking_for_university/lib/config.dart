a45ddd3fc5f13d8eea204414c2ac5df923d23a87
ChroNeoDev <chroneo47.dev@gmail.com>
2025-08-26 15:32:37 +0700

    fix(config) : แก้ config สำหรับ emulator

--- App/smart_parking_for_university/lib/config.dart
+++ App/smart_parking_for_university/lib/config.dart
@@ -1,4 +1,20 @@
+import 'dart:io' show Platform;
+
 class AppConfig {
-  static const String baseUrl = "http://localhost:3000/";
-  static const String baseApiUrl = "http://localhost:3000/api/v1";
+  static String get baseUrl {
+    if (Platform.isAndroid) {
+      return "http://10.0.2.2:3000/";
+    } else {
+      // iOS, desktop ฯลฯ ใช้ localhost ได้ตรงๆ
+      return "http://localhost:3000/";
+    }
+  }
+
+  static String get baseApiUrl {
+    if (Platform.isAndroid) {
+      return "http://10.0.2.2:3000/api/v1";
+    } else {
+      return "http://localhost:3000/api/v1";
+    }
+  }
 }
